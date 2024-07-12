466b795de95e03d0dbd2d57cb2c9bad73bfad68d
Era <era.dallashi@yahoo.com>
2024-07-12 15:47:21 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,28 @@
+
+document.getElementById('navbarBurger').addEventListener('click', function() {
+    var menu = document.getElementById('navbarMenu');
+    menu.classList.toggle('active');
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    new Glide('.glide', {
+        type: 'carousel',
+        startAt: 0,
+        perView: 2,
+        
+        animationDuration: 800
+
+        
+    }).mount();
+});
+document.querySelectorAll('.collapsible').forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('active');
+        let content = item.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + 'px';
+        }
+    });
+});
